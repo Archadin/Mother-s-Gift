e93a275297a8d380280f87556824493fe72a2dcd
Archadin <65004554+Archadin@users.noreply.github.com>
2023-05-21 23:32:35 +0300

    Player movement is done. tilemaps have a slight layering issue with the player but other than that it is good. maybe increase speed. also removed unused sprites and edited character sprites in sprite editor.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    private const string SPEED = "Speed";
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb2D;
+    private Animator animator;
 
     private Vector2 movement;
 
     // Start is called before the first frame update
     private void Start()
     {
+        animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
@@ -18,6 +24,11 @@ public class CharacterController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat(HORIZONTAL, movement.x);
+        animator.SetFloat(VERTICAL, movement.y);
+
+        animator.SetFloat(SPEED, movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
